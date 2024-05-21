b921fbd6673ecf1e46bfcb487f5ca76867226f4e
hippolyteh <frdistermin32@gmail.com>
2024-05-21 15:47:37 +0200

    first commit

---
+++ progress.py
@@ -0,0 +1,8 @@
+from IPython.display import display, clear_output
+bar = '▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊'
+empty = '                                                  '
+length = 1000
+for i in range(length):
+    clear_output(wait=True)
+    prct = round(i/length * len(bar)/100) if round(i/length * (len(bar)/100) * 100 ) > 0 else 1
+    display(f'{bar[:prct]} {empty[:-prct]}{round(i/length * 100)}%')
